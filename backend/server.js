e084ab207e6fad889811742f6b7778bfdc8b8590
Shan Mohammad <59340977+shanvoice4@users.noreply.github.com>
2025-05-16 09:38:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: "https://balanza-frontend.onrender.com",
-    credentials: true,
+    withCredentials: true,
   })
 );
 
